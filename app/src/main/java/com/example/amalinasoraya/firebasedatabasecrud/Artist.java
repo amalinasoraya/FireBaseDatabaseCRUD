f17d9d762c0d5e566bed1ac218a6c038474e06d4
Amalina Soraya <amalinasoraya@ACA823C0.ipt.aol.com>
2017-11-02 11:22:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/amalinasoraya/firebasedatabasecrud/Artist.java
@@ -0,0 +1,25 @@
+package com.example.amalinasoraya.firebasedatabasecrud;
+
+/**
+ * Created by amalinasoraya on 02/11/2017.
+ */
+
+public class Artist {
+    String artistId;
+    String artistName;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+}
